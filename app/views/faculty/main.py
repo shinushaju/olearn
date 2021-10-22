54a66fe7e0ac4f6f4daaaa293de7f6b4019778c4
shinushaju <shinushaju.ac@gmail.com>
2021-10-22 15:46:07 +0530

    updated views and added auths

--- app/views/faculty/main.py
+++ app/views/faculty/main.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import app
+from flask_login import login_required, current_user
 
 @app.route('/faculty/dashboard')
+@login_required
 def faculty_dashboard():
-    return render_template('faculty/dashboard.html')
+    return render_template('faculty/dashboard.html', user=current_user)
