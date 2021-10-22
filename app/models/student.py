c1aad583feeaacd82812e73a2dd0b355c85e58e1
shinushaju <shinushaju.ac@gmail.com>
2021-10-22 15:03:25 +0530

    updated student model

--- app/models/student.py
+++ app/models/student.py
@@ -1,13 +1,12 @@
-import re
 from flask_login import UserMixin
 from app import db
 
 # student model here
 class Student(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+    email = db.Column(db.String(24), unique=True, nullable=False)
+    password = db.Column(db.String(24), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
         return '<Student %r>' % self.name
