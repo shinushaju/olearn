1bbdf4e0e75144547d9c0ebac9c280695d407df7
shinushaju <shinushaju.ac@gmail.com>
2021-10-22 07:08:43 +0530

    added new views files

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,6 @@ db.init_app(app)
 # import main views here
 from app.views import main
 # import student views here
-from app.views.student import auth
+from app.views.student import auth, main
 # import faculty faculty views here
-from app.views.faculty import auth
+from app.views.faculty import auth, main
