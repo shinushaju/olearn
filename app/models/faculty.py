35ed2dbc1708478dc86c4cc3c7100600244ef870
shinushaju <shinushaju.ac@gmail.com>
2021-10-22 15:35:05 +0530

    added faculty model and made form updates

--- app/models/faculty.py
+++ app/models/faculty.py
@@ -1,3 +1,12 @@
+from flask_login import UserMixin
 from app import db
 
-# faculty model here
+# student model here
+class Faculty(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(24), unique=True, nullable=False)
+    password = db.Column(db.String(24), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Faculty %r>' % self.name
