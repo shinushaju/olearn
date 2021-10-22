74d51db60561f18e2b3e3da1b0b694b3c3a231b5
shinushaju <shinushaju.ac@gmail.com>
2021-10-22 09:37:31 +0530

    testing

--- app/models/student.py
+++ app/models/student.py
@@ -1,3 +1,8 @@
 from app import db
 
 # student model here
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
--- app/views/student/auth.py
+++ app/views/student/auth.py
@@ -1,11 +1,24 @@
-from flask import render_template
-from app import app
-# import db 
-from app import db
+from flask import render_template, redirect, url_for, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import app, db
+# import db model
+from app.models.student import Student
 
 # sign up as a student
-@app.route('/students/join')
+@app.route('/students/join',methods=['POST'])
 def student_signup():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+    user = Student.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+    
+    if user: # if a user is found, we want to redirect back to signup page so user can try again
+        return redirect(url_for('student_signup'))
+    else:
+        student = Student(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+        # add the new user to the database
+        db.session.add(student)
+        db.session.commit()
     return render_template('home/student-signup.html')
 
 # login as a student
