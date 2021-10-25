0932721de3162051bea532c5adb0e32f70e8900e
AtulJayan <90921402+AtulJayan@users.noreply.github.com>
2021-10-25 18:36:52 +0530

    Fixed bugs, updated views, updated model

--- app/views/course/routes.py
+++ app/views/course/routes.py
@@ -11,14 +11,15 @@ def course_page():
 @app.route('/discussions', methods=['GET', 'POST'])
 @login_required
 def course_discussions():
+    queries = Query.query.all()
     if request.method == 'POST':
         query_text = request.form['query_text']
         student_name = "Harry Potter"
         course_name = "Data science"
-        query = Query(query_text=query_text,student_name=student_name,course_name=course_name)
+        query = Query(query_text=query_text, student_name=student_name, course_name=course_name)
         db.session.add(query)
         db.session.commit()
         return redirect(url_for('course_discussions'))
-    return render_template('course/discussions.html',user=current_user)
+    return render_template('course/discussions.html', user=current_user, queries=queries)
 
 #course/<course-id>/discussion
