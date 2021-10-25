a4c141b25cc025a1e47a9ee883416aa656d1ee28
mayopik <abhiramsanthosh2k@gmail.com>
2021-10-25 12:07:01 +0530

    Abhiram Santhosh updated manage_profile view and student_details model

--- app/models/student_details.py
+++ app/models/student_details.py
@@ -1,20 +1,20 @@
 from app import db
 
-class student_details(db.Model):
-    student_id = db.Column(db.Integer, db.ForeignKey("student.id"), nullable=False, primary_key=True)
+class Student_details(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey("student.id"), nullable=False)
     student=db.relationship('Student', backref='student_details')
     roll_no=db.Column(db.String, nullable=False)
     mobile_num = db.Column(db.Integer, nullable=False)
     date_of_birth = db.Column(db.Date, nullable=False)
     student_bio = db.Column(db.String(70))
 
-    def __init__(self, **kwargs):
-        self.student_id=kwargs.get('student_id')
-        self.roll_no=kwargs.get('roll_no')
-        self.mobile_num=kwargs.get('mobile_num')
-        self.date_of_birth=kwargs.get('date_of_birth')
-        self.student_bio=kwargs.get('student_bio')
-
+    # def __init__(self, **kwargs):
+    #     self.student_id=kwargs.get('student_id')
+    #     self.roll_no=kwargs.get('roll_no')
+    #     self.mobile_num=kwargs.get('mobile_num')
+    #     self.date_of_birth=kwargs.get('date_of_birth')
+    #     self.student_bio=kwargs.get('student_bio')
 
     def __repr__(self):
         return '<student_details %r>' % self.roll_no
--- app/views/student/manage_profile.py
+++ app/views/student/manage_profile.py
@@ -1,16 +1,24 @@
-from flask import render_template, request
-from app import app, db
+from datetime import datetime
 
+from flask import render_template, request
+from flask_login import login_required, current_user
+
+from app import app, db
+from app.models.student_details import Student_details
+
+@login_required
 @app.route('/student/student_details/<id>', methods=['GET','POST'])
 def student_details(id):
     if request.method == 'POST':
-            roll_no = request.form.get('roll_no')
-            mobile_num = request.form.get('mobile_num')
-            date_of_birth = request.form.get('date_of_birth')
-            student_bio = request.form.get('student_bio')
+        roll_no = request.form.get('roll_no')
+        mobile_num = request.form.get('mobile_num')
 
-            student_detail=student_details(student_id=id, roll_no=roll_no, mobile_num=mobile_num,date_of_birth=date_of_birth,student_bio=student_bio)
-            db.session.add(student_detail)
-            db.session.commit()
-            return 'Successful'
-    return render_template('/student/student_details.html')
+        # Creating a date object from date string in form
+        date_of_birth=datetime.strptime(request.form.get('date_of_birth'), "%Y-%m-%d").date()
+
+        student_bio = request.form.get('student_bio')
+        student_detail=Student_details(student_id=id, roll_no=roll_no, mobile_num=mobile_num,date_of_birth=date_of_birth,student_bio=student_bio)
+        db.session.add(student_detail)
+        db.session.commit()
+        return 'Successful'
+    return render_template('/student/student_details.html', user=current_user)
