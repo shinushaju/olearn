e6d224d1a88d65c072fde1341ec1595d6c95bd7e
mayopik <abhiramsanthosh2k@gmail.com>
2021-10-25 12:18:55 +0530

    Abhiram Santhosh updated student_details model

--- app/models/student_details.py
+++ app/models/student_details.py
@@ -9,12 +9,5 @@ class Student_details(db.Model):
     date_of_birth = db.Column(db.Date, nullable=False)
     student_bio = db.Column(db.String(70))
 
-    # def __init__(self, **kwargs):
-    #     self.student_id=kwargs.get('student_id')
-    #     self.roll_no=kwargs.get('roll_no')
-    #     self.mobile_num=kwargs.get('mobile_num')
-    #     self.date_of_birth=kwargs.get('date_of_birth')
-    #     self.student_bio=kwargs.get('student_bio')
-
     def __repr__(self):
         return '<student_details %r>' % self.roll_no
