94659ec78ebaa21358c15389cb4988af981f9ebc
Anirudh6060 <75322578+Anirudh6060@users.noreply.github.com>
2021-10-25 16:10:59 +0530

    Updated student manage profile form

--- app/views/student/manage_profile.py
+++ app/views/student/manage_profile.py
@@ -15,14 +15,26 @@ def student_details(id):
                 # Creating a date object from date string in form
                 date_of_birth=datetime.strptime(request.form.get('date_of_birth'), "%Y-%m-%d").date()
                 student_bio = request.form.get('student_bio')
-                student_detail=Student_details(student_id=id, roll_no=roll_no, mobile_num=mobile_num,date_of_birth=date_of_birth,student_bio=student_bio)
-                db.session.add(student_detail)
-                db.session.commit()
-                return 'Successful'
-        res = Student_details.query.filter_by(id=id).one_or_none()
 
+                res = Student_details.query.filter_by(id=id).one_or_none()
+
+                if res==None:
+                        student_detail=Student_details(student_id=id, roll_no=roll_no, mobile_num=mobile_num,date_of_birth=date_of_birth,student_bio=student_bio)
+                        db.session.add(student_detail)
+                        db.session.commit()
+                        return 'Successful'
+
+                else:
+                        res.roll_no = roll_no
+                        res.mobile_num = mobile_num
+                        res.date_of_birth = date_of_birth
+                        res.student_bio = student_bio
+                        db.session.commit()
+                        return 'Successful updation'
+
+        res = Student_details.query.filter_by(id=id).one_or_none()
         if res==None:
-                form1 = {"roll_no" : "" , "mob" : "" , "dob" : "" , "sbio" : ""}
+                form1 = {"roll_no" : "" , "mob" : "" , "dob" : "" , "bio" : ""}
         else:
                 form1 = {"roll_no" : res.roll_no , "mob" : res.mobile_num , "dob" : res.date_of_birth , "bio" : res.student_bio, }
         return render_template('/student/student_details.html', user=current_user, form1=form1)
