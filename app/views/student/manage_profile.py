b4195107a3b8d6f94dd29c7f60b1698376023474
mayopik <abhiramsanthosh2k@gmail.com>
2021-10-25 11:20:43 +0530

    Abhiram Santhosh updated student_details model

--- app/views/student/manage_profile.py
+++ app/views/student/manage_profile.py
@@ -1,16 +1,16 @@
 from flask import render_template, request
 from app import app, db
 
-@app.route('/student/student_details', methods=['GET','POST'])
-def student_details():
+@app.route('/student/student_details/<id>', methods=['GET','POST'])
+def student_details(id):
     if request.method == 'POST':
             roll_no = request.form.get('roll_no')
             mobile_num = request.form.get('mobile_num')
             date_of_birth = request.form.get('date_of_birth')
             student_bio = request.form.get('student_bio')
 
-            student_detail=student_details(roll_no=roll_no, mobile_num=mobile_num,date_of_birth=date_of_birth,student_bio=student_bio)
+            student_detail=student_details(student_id=id, roll_no=roll_no, mobile_num=mobile_num,date_of_birth=date_of_birth,student_bio=student_bio)
             db.session.add(student_detail)
             db.session.commit()
             return 'Successful'
-    return render_template('/student/student-details.html')
+    return render_template('/student/student_details.html')
