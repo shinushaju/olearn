cf8430d51d1c929556d46ef154e1b064199c3e74
shinushaju <shinushaju.ac@gmail.com>
2021-10-26 13:59:54 +0530

    updated course model

--- app/models/courses.py
+++ app/models/courses.py
@@ -1,5 +1,5 @@
 from app import db
-#from app.models.faculty import Faculty
+import datetime
 
 class Course(db.Model):
 
@@ -8,8 +8,11 @@ class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     course_name = db.Column(db.String(120), nullable=False)
     course_overview = db.Column(db.String(1000), nullable=False)
+    course_skills = db.Column(db.String(720), nullable=False)
     course_duration = db.Column(db.Integer(), nullable=False)
     course_price = db.Column(db.Integer(), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    program = db.Column(db.String(150), nullable=False)
 
     faculty_id = db.Column(db.Integer, db.ForeignKey('faculty.id'),nullable=False)
     faculty = db.relationship('Faculty',backref=db.backref('faculties', cascade='all,delete-orphan', lazy=True))
