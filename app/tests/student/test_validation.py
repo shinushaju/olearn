776954322a3f9ae0a95765b8b6f41e1b75a18dd4
Anirudh6060 <75322578+Anirudh6060@users.noreply.github.com>
2021-10-26 21:44:03 +0530

    validation testing completed

---
+++ app/tests/student/test_validation.py
@@ -0,0 +1,35 @@
+from app.utils.student.validate_email import validate_email
+from app.utils.student.validate_password import validate_password
+from app.utils.student.validate_phone import validate_phone
+
+
+
+def test_validate_email():
+    assert validate_email("anirudh@tcs.com") == True
+    assert validate_email("anirudh.sasi@tcs.") == False
+    assert validate_email("anirudh.sasi@tcs.com") == True
+    assert validate_email("anirudh.sa#si@tcs.com") == False
+    assert validate_email("anirudh60.sasi60@tcs.com") == True
+    assert validate_email("anirudh.sasitcs.com") == False
+    
+    
+def test_validate_phone():
+    assert validate_phone("+91-9605475907") == True
+    assert validate_phone("+91-960547") == False
+    assert validate_phone("+91-9605475907887") == False
+    assert validate_phone("+91-(960)5475907") == True
+    assert validate_phone("+91-960547A907") == False
+    assert validate_phone("+91-96@5475907") == False
+    assert validate_phone("+91-960 547 5907") == True
+    assert validate_phone("+91-(960) 547 5907") == True
+    assert validate_phone("+1-9605475907") == True
+    assert validate_phone("+911-960 547 5907") == True
+
+
+def test_validate_password():
+    assert validate_password("Anu@3#4") == False
+    assert validate_password("0AbcA@13") == False
+    assert validate_password("abcdef@1234") == False
+    assert validate_password("abcdesd@#!") == False
+    assert validate_password("  abc  de") == False
+    assert validate_password("AnuA@1489#") == True
