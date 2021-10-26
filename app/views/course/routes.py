9703a36645d938148be6e5f7be3eed2e3c50af6c
shinushaju <shinushaju.ac@gmail.com>
2021-10-26 14:23:12 +0530

    added page to add course, created views and updated routes

--- app/views/course/routes.py
+++ app/views/course/routes.py
@@ -1,6 +1,32 @@
-from flask import render_template
-from app import app
+from flask import render_template, redirect, url_for, request, flash
+from app import app, db
+from app.models.faculty import Faculty
+from app.models.courses import Course
+from flask_login import login_required, current_user
 
 @app.route('/course')
 def course_page():
     return render_template('course/course-page.html')
+
+# create a course
+@app.route('/course/create',methods=['GET','POST'])
+@login_required
+def create_course():
+    courses = Course.query.all()
+    if request.method == 'POST':
+        course_name = request.form.get('course_name')
+        course_overview = request.form.get('course_overview')
+        course_skills = request.form.get('course_skills')
+        course_duration = request.form.get('course_duration')
+        course_price = request.form.get('course_price')
+        program = request.form.get('program')
+        faculty_id = current_user.id
+
+        if course_name and course_overview and course_skills and course_duration and course_price and program and faculty_id:
+            course = Course(course_name=course_name, course_overview=course_overview, course_skills=course_skills, course_duration=course_duration, course_price=course_price, program=program, faculty_id=faculty_id)
+            # add the new course to the database
+            db.session.add(course)
+            db.session.commit()
+            return redirect(url_for('create_course'))
+
+    return render_template('faculty/create-course.html', user=current_user, courses=courses)
