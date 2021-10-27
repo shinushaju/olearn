800e5281ae438b7a0ae9be2f76c767b9f7a78f57
Anirudh6060 <75322578+Anirudh6060@users.noreply.github.com>
2021-10-27 13:09:50 +0530

    manage profile details updated

--- app/views/student/manage_profile.py
+++ app/views/student/manage_profile.py
@@ -9,8 +9,8 @@ from app.utils.student.validate_phone import validate_phone
 
 
 @login_required
-@app.route('/student/student_details/<id>', methods=['GET','POST'])
-def student_details(id):
+@app.route('/student/student_details', methods=['GET','POST'])
+def student_details():
         if request.method == 'POST':
                 roll_no = request.form.get('roll_no')
                 mobile_num = request.form.get('mobile_num')
@@ -18,14 +18,15 @@ def student_details(id):
                 date_of_birth=datetime.strptime(request.form.get('date_of_birth'), "%Y-%m-%d").date()
                 student_bio = request.form.get('student_bio')
  
-                res = Student_details.query.filter_by(id=id).one_or_none()
+                res = Student_details.query.filter_by(student_id=current_user.id).one_or_none()
 
                 if validate_phone(mobile_num)==True:
                         if res==None:
-                                student_detail=Student_details(student_id=id, roll_no=roll_no, mobile_num=mobile_num,date_of_birth=date_of_birth,student_bio=student_bio)
+                                student_detail=Student_details(student_id=current_user.id, roll_no=roll_no, mobile_num=mobile_num,date_of_birth=date_of_birth,student_bio=student_bio)
                                 db.session.add(student_detail)
                                 db.session.commit()
-                                return redirect(url_for('student_dashboard'))
+                                flash("Details added successfully", 'success')
+                                return redirect(url_for('student_details'))
 
                         else:
                                 res.roll_no = roll_no
@@ -33,11 +34,12 @@ def student_details(id):
                                 res.date_of_birth = date_of_birth
                                 res.student_bio = student_bio
                                 db.session.commit()
-                                return redirect(url_for('student_dashboard'))
+                                flash("Details updated successfully", 'success')
+                                return redirect(url_for('student_details'))
                 else:
-                        flash('Enter a valid Mobile Number')
+                        flash('Enter a valid Mobile Number', 'error')
                         
-        res = Student_details.query.filter_by(id=id).one_or_none()
+        res = Student_details.query.filter_by(student_id=current_user.id).one_or_none()
         
         if res==None:
                 #Entering for first time- Return empty string to form
