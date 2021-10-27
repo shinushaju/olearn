0acc580e0860c15299c574e3962dea662adbc4ce
mayopik <abhiramsanthosh2k@gmail.com>
2021-10-27 16:26:30 +0530

    Abhiram Santhosh integrated search feature from Abhiram

--- app/views/student/search.py
+++ app/views/student/search.py
@@ -1,4 +1,3 @@
-import re
 from flask import render_template, request
 from flask_login import login_required, current_user
 
@@ -8,18 +7,17 @@ from app.models.courses import Course
 @login_required
 @app.route('/student/search')
 def search():
-    form=dict()
     # Collecting GET parameters
-    form['q']=request.args.get('q')
+    q=request.args.get('q')
     # searchFilter=request.args.get('filter')
-    form['duration']=request.args.get('duration')
+    duration=request.args.get('duration')
 
     # Note: All searches are case-insensitive
 
     # Search by course name
-    results=Course.query.filter(Course.course_name.ilike('%'+"%".join(form['q'].split(" "))+'%'))
+    results=Course.query.filter(Course.course_name.ilike('%'+"%".join(q.split(" "))+'%')).all()
     # Filter by course duration
-    if form['duration'] is not None:
-        results=Course.query.filter(Course.course_name.ilike('%'+"%".join(form['q'].split(" "))+'%') & (Course.course_duration<=int(form['duration'])))
+    if (duration is not None) and (duration!='any'):
+        results=Course.query.filter(Course.course_name.ilike('%'+"%".join(q.split(" "))+'%') & (Course.course_duration<=int(duration))).all()
 
-    return render_template('student/search.html', courses=results, user=current_user, form=form)
+    return render_template('student/search.html', courses=results, user=current_user)
