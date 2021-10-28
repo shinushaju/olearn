5c907612efc8d93be947cab2f82461ff3a1fd357
mayopik <abhiramsanthosh2k@gmail.com>
2021-10-28 11:26:35 +0530

    Abhiram Santhosh modified EnrolledCourses, Student_details models

--- app/models/enrolledCourses.py
+++ app/models/enrolledCourses.py
@@ -4,9 +4,9 @@ from app import db
 class Enrolled_courses(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     student_id=db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
-    student=db.relationship('Student', backref='enrolled_courses')
+    student=db.relationship('Student', backref='enrolled_courses', cascade='all,delete-orphan')
     course_id=db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
-    course=db.relationship('Course', backref='enrolled_courses')
+    course=db.relationship('Course', backref='enrolled_courses', cascade='all,delete-orphan')
     progress=db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
