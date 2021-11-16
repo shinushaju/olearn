b198826403aeaaef4b5c5f09526b9cb2aef9a9ac
mayopik <abhiramsanthosh2k@gmail.com>
2021-11-16 11:51:06 +0530

    Abhiram added student role required decorator

--- app/models/student.py
+++ app/models/student.py
@@ -7,6 +7,10 @@ class Student(UserMixin, db.Model):
     email = db.Column(db.String(24), unique=True, nullable=False)
     password = db.Column(db.String(24), nullable=False)
     name = db.Column(db.String(50), nullable=False)
+    role = db.Column(db.String(50), nullable=False, default="student")
 
     def __repr__(self):
         return '<Student %r>' % self.name
+
+    def is_student(self):
+        return self.role == 'student'
