f0bb97e5506af22d4a6dd0397c0a3b374df7859e
mayopik <abhiramsanthosh2k@gmail.com>
2021-11-24 12:37:51 +0530

    Abhiram fixed and merged faculty-profile from Atul

--- app/models/faculty.py
+++ app/models/faculty.py
@@ -8,6 +8,10 @@ class Faculty(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
     faculty_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     name = db.Column(db.String(50), nullable=False)
+    phoneNo = db.Column(db.Integer)
+    qualification = db.Column(db.String(50))
+    address = db.Column(db.String(50))
+    gender = db.Column(db.String(20), default="NIL")
 
     # many to one relationship with class user
     user = db.relationship("User", back_populates="faculty", cascade="all, delete")
--- app/views/faculty/main.py
+++ app/views/faculty/main.py
@@ -1,9 +1,11 @@
-from app import app
-from flask import render_template, session
+from app import app, db
+from flask import render_template, request, flash
 from flask_login import login_required, current_user
+from werkzeug.security import generate_password_hash
 from app.models.courses import Course
 from app.utils.decorators import faculty_role_required
 from app.models.faculty import Faculty
+from app.utils.auth import validate_email, validate_name, validate_password
 ### Faculty Dashboard View - only user with faculty role has access.
 @app.route('/faculty/dashboard')
 @login_required
@@ -33,3 +35,54 @@ def faculty_courses():
 def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
+
+### Faculty Profile
+@app.route('/faculty_profile', methods=['GET', 'POST'])
+@login_required
+@faculty_role_required()
+def faculty_profile():
+    user = current_user
+    faculty=user.faculty
+    flash(faculty.name, 'debug')
+    names = faculty.name.split()
+    if request.method == 'POST':
+        name=faculty.name
+        if request.form.get('Name1') is not None and request.form.get('Name2') is not None:
+            if validate_name(request.form.get('Name1')) and validate_name(request.form.get('Name2')):
+                name = request.form.get('Name1')+" "+request.form.get('Name2')
+            else:
+                flash('Invalid name!', 'error')
+
+        email=user.email
+        if request.form.get('email') is not None:
+            if validate_email(request.form.get('email')):
+                email = request.form.get('email') or user.email
+            else:
+                flash('Invalid email!', 'error')
+
+        gender = request.form.get('gender') or faculty.gender
+        phoneNo = request.form.get('mobileNumber') or faculty.phoneNo
+        address = request.form.get('address') or faculty.address
+        qualification = request.form.get('qualification') or faculty.qualification
+        if request.form.get('password') is not None:
+            if validate_password(request.form.get('password')):
+                new_password=request.form.get('password')
+                user.password=generate_password_hash(new_password, method='sha256')
+            else:
+                flash("Password not valid!", 'error')
+        user.email = email
+        faculty.name = name
+        faculty.gender = gender
+        faculty.phoneNo = phoneNo
+        faculty.address = address
+        faculty.qualification = qualification
+        db.session.add(user)
+        db.session.commit()
+        db.session.add(faculty)
+        db.session.commit()
+        names
+        return render_template('faculty/faculty_profile.html', user=user, names=name.split())
+
+    return render_template('faculty/faculty_profile.html',user=user, names=names)
+
+
